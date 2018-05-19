b5fcd365d9b78bbd9c37ab5c52558839762aad1c
Erik <vfxbaliof@gmail.com>
2018-05-19 18:43:54 +0300

    01. Triangle of Stars

---
+++ FUNCTIONS/01. Triangle of Stars/01. Triangle of Stars.js
@@ -0,0 +1,10 @@
+function solution(n){
+    for (let i = 1; i <= n; i++) {
+        console.log('*'.repeat(i));
+    }
+    for (let i = n - 1; i >= 1; i--) {
+        console.log('*'.repeat(i));
+    }
+}
+
+solution(5);
