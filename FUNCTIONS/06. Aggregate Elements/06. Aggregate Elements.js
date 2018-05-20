bdf3ae6677c35857dc1968d67ff40cdf2ed8be89
Erik <vfxbaliof@gmail.com>
2018-05-20 10:48:46 +0300

    06. Aggregate Elements

---
+++ FUNCTIONS/06. Aggregate Elements/06. Aggregate Elements.js
@@ -0,0 +1,15 @@
+function solution(elements){
+
+    aggregate(elements, 0, (a, b) => a + b);
+    aggregate(elements, 0, (a, b) => a + 1 / b);
+    aggregate(elements, '', (a, b) => a + b);
+
+    function aggregate(arr, initVal, func){
+        for (let i = 0; i < arr.length; i++) {
+            initVal = func(initVal, arr[i]);
+        }
+        console.log(initVal);
+    }
+}
+
+solution([1, 2, 3]);
