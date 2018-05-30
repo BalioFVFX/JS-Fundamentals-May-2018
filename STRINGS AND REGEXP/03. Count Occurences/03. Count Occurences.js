5a812162e2de43f6b1a687f5a2dde185d420691a
Erik <vfxbaliof@gmail.com>
2018-05-30 11:55:34 +0300

    03. Count Occurences

---
+++ STRINGS AND REGEXP/03. Count Occurences/03. Count Occurences.js
@@ -0,0 +1,9 @@
+function solution(target, text) {
+    let counter = 0;
+    let index = text.indexOf(target);
+    while (index !== -1) {
+        counter++;
+        index = text.indexOf(target, index + 1);
+    }
+    console.log(counter);
+}
