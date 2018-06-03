9f6b0af460d634023b2b82aa99210b73993ffd04
Erik <vfxbaliof@gmail.com>
2018-06-03 11:53:39 +0300

    10. Extract Unique Words

--- OBJECTS AND JSON/10. Extract Unique Words/10. Extract Unique Words.js
+++ OBJECTS AND JSON/10. Extract Unique Words/10. Extract Unique Words.js
@@ -8,11 +8,3 @@ function solution(input) {
     }
     console.log(Array.from(set).join(', '));
 }
-
-solution(['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque quis hendrerit dui. \n' +
-'Quisque fringilla est urna, vitae efficitur urna vestibulum fringilla. \n' +
-'Vestibulum dolor diam, dignissim quis varius non, fermentum non felis. \n' +
-'Vestibulum ultrices ex massa, sit amet faucibus nunc aliquam ut. \n' +
-'Morbi in ipsum varius, pharetra diam vel, mattis arcu. \n' +
-'Integer ac turpis commodo, varius nulla sed, elementum lectus. \n' +
-'Vivamus turpis dui, malesuada ac turpis dapibus, congue egestas metus.']);
